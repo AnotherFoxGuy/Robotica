91115211c26328bdbd24d87d65a76b5b597173e2
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-21 16:06:31 +0200

    :art: Added clang-format

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,6 @@
-#include <webots/Robot.hpp>
 #include <webots/DistanceSensor.hpp>
 #include <webots/Motor.hpp>
+#include <webots/Robot.hpp>
 
 // time in [ms] of a simulation step
 #define TIME_STEP 64
@@ -11,31 +11,33 @@
 using namespace webots;
 
 // entry point of the controller
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     // create the Robot instance.
-    Robot *robot = new Robot();
+    Robot* robot = new Robot();
 
     // initialize devices
-    DistanceSensor *ps[8];
+    DistanceSensor* ps[8];
     char psNames[8][4] = {
-            "ps0", "ps1", "ps2", "ps3",
-            "ps4", "ps5", "ps6", "ps7"
-    };
+        "ps0", "ps1", "ps2", "ps3",
+        "ps4", "ps5", "ps6", "ps7"};
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++)
+    {
         ps[i] = robot->getDistanceSensor(psNames[i]);
         ps[i]->enable(TIME_STEP);
     }
 
-    Motor *leftMotor = robot->getMotor("left wheel motor");
-    Motor *rightMotor = robot->getMotor("right wheel motor");
+    Motor* leftMotor = robot->getMotor("left wheel motor");
+    Motor* rightMotor = robot->getMotor("right wheel motor");
     leftMotor->setPosition(INFINITY);
     rightMotor->setPosition(INFINITY);
     leftMotor->setVelocity(0.0);
     rightMotor->setVelocity(0.0);
 
     // feedback loop: step simulation until an exit event is received
-    while (robot->step(TIME_STEP) != -1) {
+    while (robot->step(TIME_STEP) != -1)
+    {
         // read sensors outputs
         double psValues[8];
         for (int i = 0; i < 8; i++)
@@ -43,23 +45,26 @@ int main(int argc, char **argv) {
 
         // detect obstacles
         bool right_obstacle =
-                psValues[0] > 80.0 ||
-                psValues[1] > 80.0 ||
-                psValues[2] > 80.0;
+            psValues[0] > 80.0 ||
+            psValues[1] > 80.0 ||
+            psValues[2] > 80.0;
         bool left_obstacle =
-                psValues[5] > 80.0 ||
-                psValues[6] > 80.0 ||
-                psValues[7] > 80.0;
+            psValues[5] > 80.0 ||
+            psValues[6] > 80.0 ||
+            psValues[7] > 80.0;
 
         // initialize motor speeds at 50% of MAX_SPEED.
         double leftSpeed = 0.5 * MAX_SPEED;
         double rightSpeed = 0.5 * MAX_SPEED;
         // modify speeds according to obstacles
-        if (left_obstacle) {
+        if (left_obstacle)
+        {
             // turn right
             leftSpeed += 0.5 * MAX_SPEED;
             rightSpeed -= 0.5 * MAX_SPEED;
-        } else if (right_obstacle) {
+        }
+        else if (right_obstacle)
+        {
             // turn left
             leftSpeed -= 0.5 * MAX_SPEED;
             rightSpeed += 0.5 * MAX_SPEED;
