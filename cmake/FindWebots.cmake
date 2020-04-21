f4736d8163df794101675c8fbc3d90392bf7d7e4
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-21 11:57:40 +0200

    :tada: Initial commit

---
+++ cmake/FindWebots.cmake
@@ -0,0 +1,28 @@
+include(FindPackageHandleStandardArgs)
+
+####################################################################################################
+#  Default (c)
+####################################################################################################
+
+find_path(Webots_C_INCLUDE_DIRS Lidar.h
+        HINTS $ENV{WEBOTS_HOME}/include/controller/c/
+        PATH_SUFFIXES webots
+        )
+
+find_library(Webots_C_LIBRARIES
+        NAMES Controller libController
+        HINTS $ENV{WEBOTS_HOME}/lib/controller
+        )
+
+find_package_handle_standard_args(Webots FOUND_VAR Webots_FOUND
+        REQUIRED_VARS Webots_C_INCLUDE_DIRS Webots_C_LIBRARIES
+        )
+
+if (Webots_FOUND)
+    add_library(Webots::Controller INTERFACE IMPORTED)
+    set_target_properties(Webots::Controller PROPERTIES
+            INTERFACE_LINK_LIBRARIES "${Webots_C_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORIES "${Webots_C_INCLUDE_DIRS}"
+            )
+endif ()
+mark_as_advanced(Webots_C_LIBRARIES Webots_C_INCLUDE_DIRS)
---
+++ source/main.cpp
@@ -0,0 +1,15 @@
+#include <Robot.hpp>
+#include <iostream>
+
+using namespace webots;
+
+int main()
+{
+    Robot *robot = new Robot();
+
+    while (robot->step(32) != -1)
+        std::cout << "Hello World!" << std::endl;
+
+    delete robot;
+    return 0;
+}
