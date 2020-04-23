290c68a3dbe4abbea24181e575c21cf22a1c5f89
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-23 15:40:08 +0200

    :tada: Added webview

---
+++ source/WebViewManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/WebSocket.h>
+
+class WebViewManager
+{
+  public:
+    void start();
+    void stop();
+    void sendData(const std::string& payload);
+  private:
+    Poco::Net::HTTPServer *server;
+
+};
+
