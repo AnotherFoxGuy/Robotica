3585feaa0ba6856d20c51b035e210612235ce6ab
ClawmanCat <clawmancat@gmail.com>
2020-06-08 22:52:32 +0200

    Fix joystick inputs not being scaled

--- source/comms/joystick_callback.hpp
+++ source/comms/joystick_callback.hpp
@@ -16,8 +16,8 @@ namespace robotica {
         auto args = regex_groups(std::string(argstring), rgx);
 
         auto& settings = main_window::instance();
-        settings.left_motor  = std::clamp(std::stoi(args[1]), -100, 100);
-        settings.right_motor = std::clamp(std::stoi(args[2]), -100, 100);
+        settings.left_motor  = 0.005 * std::clamp(std::stoi(args[1]), -100, 100);
+        settings.right_motor = 0.005 * std::clamp(std::stoi(args[2]), -100, 100);
 
         std::cout << "Robot movement speed was changed by joystick input.\n";
     }
