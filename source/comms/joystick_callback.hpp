22649ca9a80881e0811dac4caa8adba4ca0ddabe
JanSHoekstra <altlockg@gmail.com>
2020-06-10 20:07:07 +0200

    Adjust movement for better controlability from controller.

--- source/comms/joystick_callback.hpp
+++ source/comms/joystick_callback.hpp
@@ -1,24 +1,27 @@
 #pragma once
 
-#include <world/robot.hpp>
 #include <window/main_window.hpp>
+#include <world/robot.hpp>
 
-#include <string_view>
-#include <regex>
-#include <string>
 #include <algorithm>
 #include <iostream>
+#include <regex>
+#include <string>
+#include <string_view>
 
+namespace robotica
+{
+inline void joystick_callback(std::string_view argstring)
+{
+    const static std::regex rgx{R"REGEX(\s*(-?\d+)\s*,\s*(-?\d+)\s*)REGEX"};
+    auto args = regex_groups(std::string(argstring), rgx);
 
-namespace robotica {
-    inline void joystick_callback(std::string_view argstring) {
-        const static std::regex rgx { R"REGEX(\s*(-?\d+)\s*,\s*(-?\d+)\s*)REGEX" };
-        auto args = regex_groups(std::string(argstring), rgx);
+    auto& settings = main_window::instance();
+    int x = std::stoi(args[1]) + std::stoi(args[2]);
+    int y = std::stoi(args[2]) - std::stoi(args[1]);
+    settings.left_motor = 0.01 * std::clamp(x, -100, 100);
+    settings.right_motor = 0.01 * std::clamp(y, -100, 100);
 
-        auto& settings = main_window::instance();
-        settings.left_motor  = 0.005 * std::clamp(std::stoi(args[1]), -100, 100);
-        settings.right_motor = 0.005 * std::clamp(std::stoi(args[2]), -100, 100);
-
-        std::cout << "Robot movement speed was changed by joystick input.\n";
-    }
+    std::cout << "Robot movement speed was changed by joystick input.\n";
 }
+} // namespace robotica
