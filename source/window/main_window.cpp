2af1d4debdfc87b68371635e9a39c2d19a69ced5
ClawmanCat <hhy.yhh@live.nl>
2020-06-15 11:22:51 +0200

    Add imgui-plot

--- source/window/main_window.cpp
+++ source/window/main_window.cpp
@@ -7,6 +7,7 @@
 #include <vision/converters.hpp>
 #include <vision/world_model.hpp>
 #include <comms/websocket.hpp>
+#include <window/imgui_plot/imgui_plot.hpp>
 
 #include <opencv2/highgui.hpp>
 #include <magic_enum.hpp>
