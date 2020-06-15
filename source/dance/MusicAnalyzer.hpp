d26800a1f7c8ed7d3ef00bdaa9a2dbe0830c924d
dante <danteklijn01@student.nhlstenden.com>
2020-06-15 15:04:35 +0200

    added line dance files and hz meter
    hz meter is probably not working correctly

--- source/dance/MusicAnalyzer.hpp
+++ source/dance/MusicAnalyzer.hpp
@@ -3,33 +3,52 @@
 class MusicAnalyzer
 {
     AudioFile<double> audioFile;
-public:
-    bool loadfile() { return audioFile.load("E:/Robotica/kaas.wav");}
-    
-    void printInfo() { audioFile.printSummary();}
+
+  public:
+    bool loadfile() { return audioFile.load("E:/Robotica/music/wariors.wav"); }
+
+    void printInfo() { audioFile.printSummary(); }
     void getSampels()
     {
-        double avrageSamples = 0;
-        int length = 0;
-        int channel = 0; //audioFile.getNumChannels();
-        int numSamples = audioFile.getNumSamplesPerChannel();
+        double prefiousSamples = 0;
+        bool goingDown = true;
+        int channel = audioFile.getNumChannels();
+        int hz[2]{0, 0};
+        int maxhz = 0;
+        int minhz = 10000;
+        const int numSamples = audioFile.getNumSamplesPerChannel();
         for (int j = 0; j < numSamples; j++)
         {
-            //for (int i = 0; i < channel; i++)
-            //{
-                double currentSample = audioFile.samples[channel][j];
-                avrageSamples += currentSample * audioFile.getBitDepth();
-      
-            //}
+            for (int i = 0; i < channel; i++)
+            {
+                double currentSample = audioFile.samples[i][j];
+                // printf("sample %f \n", currentSample);
+                if (prefiousSamples > currentSample && currentSample > 0 && goingDown)
+                {
+                    hz[i]++;
+                    goingDown = false;
+                }
+                if (prefiousSamples < 0 && currentSample > 0)
+                    goingDown = true;
+
+                prefiousSamples = currentSample;
+                //printf("%f \n", currentSample);
+            }
             if (j % audioFile.getSampleRate() == 0 && j != 0)
             {
-                int sec = j/ audioFile.getSampleRate();
-                
-                printf("sample %f sec: %d \n", (avrageSamples / audioFile.getSampleRate()) * audioFile.getSampleRate(),sec);
-                avrageSamples = 0;
-            }
-            
+                for (int i = 0; i < audioFile.getNumChannels(); i++)
+                {
+                    printf("hz[%d]: %d ", i, hz[i]);
+                    if (hz[i] > maxhz)
+                        maxhz = hz[i];
+                    if (hz[i] < minhz)
+                        minhz = hz[i];
 
+                    hz[i] = 0;
+                }
+                printf("\n");
+            }
         }
+        printf("minhz: %d maxhz: %d", minhz, maxhz);
     }
 };
