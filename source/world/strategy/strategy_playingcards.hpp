8c8ac00d9cf5a6090d5b042328cf6e66ba997edc
OutForSomeCode <43600545+OutForSomeCode@users.noreply.github.com>
2020-06-17 14:45:35 +0200

    :wrench: Card strat updated (#65)

--- source/world/strategy/strategy_playingcards.hpp
+++ source/world/strategy/strategy_playingcards.hpp
@@ -12,26 +12,44 @@ class strategy_playingcards : public istrategy {
   public:
     strategy_playingcards(std::string card) {
         playingcard = card;
+        speed = main_window::instance().speed;
+        neighbours = main_window::instance().min_neighbours;
     }
 
     void init(void) override {
-        main_window::instance().speed = 10;
+        main_window::instance().speed = 50;
         main_window::instance().min_neighbours = 10;
     }
 
     void exit(void) override {
-        main_window::instance().speed = 1;
-        main_window::instance().min_neighbours = 64;
+        main_window::instance().speed = speed;
+        main_window::instance().min_neighbours = neighbours;
     }
 
     void loop(void) override {
-        auto& objects = world_model::instance().get_object_list();
-        if (auto it = std::find_if(objects.begin(), objects.end(), [&](const auto& o) { return o.identity == playingcard;}); it != objects.end() && !resetting) {
-            main_window::instance().left_motor = 1;
-            main_window::instance().right_motor = 1;
+        auto& objects = world_model::instance().get_raw_object_list();
+        if (auto it = std::find_if(objects.begin(), objects.end(), [&](const auto& o) { return o.type == playingcard;}); it != objects.end() && !resetting) {
+            auto center = it->bounding_rect.x + (it->bounding_rect.width / 2);
 
-            lookingForTarget = false;
-            ++ticks;
+            if(center > 121 && center < 124 && lookingForTarget) {
+                lookingForTarget = false;
+            } else if (center < 121 && lookingForTarget) {
+                main_window::instance().speed = 5;
+                main_window::instance().left_motor = -1;
+                main_window::instance().right_motor = 1;
+            } else if(center > 124 && lookingForTarget) {
+                main_window::instance().speed = 5;
+                main_window::instance().left_motor = 1;
+                main_window::instance().right_motor = -1;
+            } else if (ticks > 75) {
+                resetting = true;
+            }else if(!lookingForTarget) {
+                main_window::instance().speed = 50;
+                main_window::instance().left_motor = 1;
+                main_window::instance().right_motor = 1;
+
+                ++ticks;
+            }
         } else if(lookingForTarget) {
             main_window::instance().left_motor = -1;
             main_window::instance().right_motor = 1;
@@ -39,7 +57,6 @@ class strategy_playingcards : public istrategy {
             main_window::instance().left_motor = -1;
             main_window::instance().right_motor = -1;
 
-            resetting = true;
             --ticks;
         } else {
             main_window::instance().left_motor = 0;
@@ -49,6 +66,8 @@ class strategy_playingcards : public istrategy {
 
   private:
     std::string playingcard;
+    int speed;
+    int neighbours;
     std::size_t ticks = 0;
     bool resetting = false;
     bool lookingForTarget = true;
