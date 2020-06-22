d8f2c2aadcc97280714a6cd80898e820d0cfc275
Roland <roland_zwart@outlook.com>
2020-06-22 18:33:40 +0200

    auto screenshots disabled

--- source/window/main_window.cpp
+++ source/window/main_window.cpp
@@ -227,8 +227,7 @@ namespace robotica {
             image_to_file(snapshot_folder.string(), "POSITIVE_"s + datetime_string(), left.get_image());
         }
 
-        static int tick = 0;
-        if (ImGui::Button("Snap Negative", { 200, 30 }) || tick++ % 30 == 0) {
+        if (ImGui::Button("Snap Negative", { 200, 30 })) {
             image_to_file(snapshot_folder.string(), "NEGATIVE_"s + datetime_string(), left.get_image());
         }
 
