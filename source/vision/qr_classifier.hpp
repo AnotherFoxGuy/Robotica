566eea061ed4f9ce55ff2f39d24f16fd1c0a05dd
dante <danteklijn01@student.nhlstenden.com>
2020-06-22 23:08:39 +0200

    :art:

--- source/vision/qr_classifier.hpp
+++ source/vision/qr_classifier.hpp
@@ -1,50 +1,46 @@
 #pragma once
-#include <vision/iclassifier.hpp>
 #include <utility/typedefs.hpp>
+#include <vision/iclassifier.hpp>
 #include <window/main_window.hpp>
 
 #include <opencv2/core.hpp>
-#include <opencv2/objdetect.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
-#include <string_view>
+#include <cassert>
 #include <filesystem>
 #include <string>
-#include <cassert>
+#include <string_view>
 #include <vector>
 namespace robotica
 {
-    class qr_classifier : public robotica::iclassifier
+class qr_classifier : public iclassifier
+{
+  public:
+    qr_classifier() : iclassifier("qr"),filename(filename),classifier((classifier_folder / filename).string()){}
+
+    std::vector<classified_object> classify(const cv::Mat& image) const override
     {
-      public:
-        qr_classifier() : iclassifier("qr"),
-                                                                                  filename(filename),
-                                                                                  classifier((classifier_folder / filename).string())
+        auto& settings = main_window::instance();
+
+        cv::QRCodeDetector qr_detector = cv::QRCodeDetector::QRCodeDetector();
+        cv::Mat bbox, rectifiedImage;
+        std::string data = qr_detector.detectAndDecode(image, bbox, rectifiedImage);
+        if (data.length() > 0)
         {
+            std::cout << "decoded data:" << data << std::endl;
+            rectifiedImage.convertTo(rectifiedImage, CV_8UC3);
+            imshow("Rectified QRCode", rectifiedImage);
         }
+        else
+            std::cout << "Qr not found" << std::endl;
+        return {};
+    }
 
-        std::vector<classified_object> classify(const cv::Mat& image) const override
-        {
-            auto& settings = main_window::instance();
 
-            cv::QRCodeDetector qr_detector = cv::QRCodeDetector::QRCodeDetector();
-            cv::Mat bbox, rectifiedImage;
-            std::string data = qr_detector.detectAndDecode(image, bbox, rectifiedImage);
-            if (data.length() > 0)
-            {
-                std::cout << "decoded data:" << data << std::endl;
-                rectifiedImage.convertTo(rectifiedImage, CV_8UC3);
-                imshow("Rectified QRCode", rectifiedImage);
-            }
-            else
-                std::cout << "Qr not found" << std::endl;
-            return {};
-        }
-        
-
-      private:
-        std::string filename;
-        mutable cv::CascadeClassifier classifier; // Not const correct.
-    };
-}
+  private:
+    std::string filename;
+    mutable cv::CascadeClassifier classifier; // Not const correct.
+};
+} // namespace robotica
